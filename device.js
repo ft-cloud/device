7fbe49e8302b6284dff8ab679bbc36b3bb06d356
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-08-18 22:29:34 +0200

    Added Debug Message

--- device.js
+++ device.js
@@ -226,7 +226,7 @@ var device = {
 
                     } else {
                         axios("http://account:3000/api/v1/account/isUserAdmin?uuid="+useruuid).then(parsed => {
-
+                            console.log(parsed.isAdmin)
                             if (parsed.isAdmin) {
                                 var sql = `SELECT name, uuid, config, deviceUUID, online, statusInfo
                                            FROM deviceData d
